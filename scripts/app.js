9caf65bc558974b2c449cd151365e965e5315ecd
mrogach2350 <m.rogach@gmail.com>
2017-03-02 19:03:18 -0800

    removes old consoles

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,15 @@
 console.log("Sanity Check");
-angular.module('IsItRotten', [])
+angular.module('IsItRotten', ['djds4rce.angular-socialshare'])
+  .config(config)
+  .run(function($FB){
+    $FB.init('1271911079507707')
+  });
+
+  config.$inject = ['$locationProvider'];
+  function config ($locationProvider){
+    $locationProvider
+      .html5Mode({
+          enabled: true,
+          requireBase: false
+      });
+  };
